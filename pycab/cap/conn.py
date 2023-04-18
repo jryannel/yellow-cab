7affe69e355d4e7670f8b1cedc65c0c51a0dfc2f
Jürgen Ryannel <juergen@ryannel.org>
2023-04-18 10:57:05 +0200

    some changes, not tested

--- pycab/cap/conn.py
+++ pycab/cap/conn.py
@@ -18,47 +18,9 @@ class Conn:
     async def close(self):
         await self.nc.close()
 
-    async def subscribe(self, objectId):
-        return await self.ensure_object(objectId).subscribe()
-
-    async def unsubscribe(self, objectId):
-        return await self.ensure_object(objectId).unsubscribe()
-
-    async def request_method(self, objectId, member, args):
-        return await self.ensure_object(objectId).request_method(member, args)
-    
-    def register_method(self, objectId, member, method):
-        return self.ensure_object(objectId).register_method(member, method)
-    
-    async def request_event(self, event):
-        return await self.ensure_object(event.objectId).request_event(event)
-    
-    async def publish_signal(self, objectId, member, args):
-        return await self.ensure_object(objectId).publish_signal(member, args)
-    
-    async def emit_signal(self, objectId, member, args):
-        return await self.ensure_object(objectId).emit_signal(member, args)
-    
-    async def on_signal(self, objectId, member, cb):
-        return await self.ensure_object(objectId).on_signal(member, cb)
-    
-    async def publish_property(self, objectId, member, value):
-        return await self.ensure_object(objectId).publish_property(member, value)
-    
-    async def emit_property(self, objectId, member, value):
-        return await self.ensure_object(objectId).emit_property(member, value)
-    
-    async def on_property(self, objectId, member, cb):
-        return await self.ensure_object(objectId).on_property(member, cb)
-    
-    async def set_property(self, objectId, member, value):
-        return await self.ensure_object(objectId).set_property(member, value)
-    
-    async def get_property(self, objectId, member):
-        return await self.ensure_object(objectId).get_property(member)
-    
-    async def get_properties(self, objectId):
-        return await self.ensure_object(objectId).get_properties()
+    def new_object(self, objectId):
+        o = object.Object(self.nc, objectId)
+        self.objs[objectId] = o
     
     def get_object_ids(self):
         return self.objs.keys()
--- pycab/cap/test_conn.py
+++ pycab/cap/test_conn.py
@@ -15,8 +15,9 @@ async def test_subscribe():
     addr = "nats://localhost:4222"
     c = conn.Conn(addr)
     await c.connect()
-    await c.subscribe("demo.calc")
-    await c.unsubscribe("demo.calc")
+    o = c.new_object("demo.calc")
+    await o.subscribe("demo.calc")
+    await o.unsubscribe("demo.calc")
     await c.close()
 
 @pytest.mark.asyncio
@@ -24,12 +25,13 @@ async def test_request_method():
     addr = "nats://localhost:4222"
     c = conn.Conn(addr)
     await c.connect()
-    await c.subscribe("demo.calc")
+    o = c.new_object("demo.calc")
+    await o.subscribe("demo.calc")
     def add(args):
         return sum(args)
     c.register_method("demo.calc", "add", add)
-    result = await c.request_method("demo.calc", "add", [1, 2])
+    result = await o.request_method("demo.calc", "add", [1, 2])
     assert result == 3
-    await c.unsubscribe("demo.calc")
+    await o.unsubscribe("demo.calc")
     await c.close()
 
